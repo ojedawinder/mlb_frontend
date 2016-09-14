ff14853c9c55dd34ac51d4547861dc5816d9a61e
Winder Ojeda <winder.alejandro@gmail.com>
2016-09-14 01:53:38 -0400

    se agregan select para seleccionar equipos

--- app/scripts/controllers/stats.js
+++ app/scripts/controllers/stats.js
@@ -9,10 +9,49 @@
  */
 angular.module('mlbApp')
   .controller('StatsCtrl', ['$scope', 'TeamService', 'GameService', function ($scope, TeamService, GameService) {
-    var team1 = 111;
-    var team2 = 141;
-    var year = 2016;
-    GameService.stats_rivals_by_year(team1, team2, year).then(function(data){
-      $scope.list = data;
-    });
+
+    $scope.year = 2016;
+
+    $scope.getStarted = function(){
+      TeamService.allTeams().then(function(data){
+        $scope.team_list = data;
+        $scope.homeSelected= { value: data[0] };
+        $scope.awaySelected = { value: data[1] };
+        $scope.team1 = data[0];
+        $scope.team2 = data[1];
+        $scope.stats_rivals_by_year($scope.team1.id, $scope.team2.id, $scope.year);
+        $scope.stats_rivals_avg_run_and_rhe_by_year($scope.team1.id, $scope.team2.id, $scope.year);
+      });
+    }
+
+    $scope.onAwaySelected = function(awayTeam){
+      $scope.team2 = awayTeam;
+      $scope.stats_rivals_by_year($scope.team1.id, awayTeam.id, $scope.year);
+      $scope.stats_rivals_avg_run_and_rhe_by_year($scope.team1.id, awayTeam.id, $scope.year);
+    }
+
+    $scope.onHomeSelected = function(homeTeam){
+      $scope.team1 = homeTeam;
+      $scope.stats_rivals_by_year(homeTeam.id, $scope.team2.id, $scope.year);
+      $scope.stats_rivals_avg_run_and_rhe_by_year(homeTeam.id, $scope.team2.id, $scope.year);
+    }
+
+
+    $scope.stats_rivals_by_year = function(team1Id, team2Id, year){
+      GameService.stats_rivals_by_year(team1Id, team2Id, year).then(function(data){
+        $scope.list = data;
+      });
+    }
+
+    $scope.stats_rivals_avg_run_and_rhe_by_year = function(team1Id, team2Id, year){
+      GameService.stats_rivals_avg_run_and_rhe_by_year(team1Id, team2Id, year).then(function(data){
+        $scope.statistics = data[0];
+        $scope.team1 = $scope.statistics.home_team;
+        $scope.team2 = $scope.statistics.away_team;
+      });
+    }
+
+    $scope.getStarted();
+
+
   }]);
